f25d942f9bf46c85eb2263d26d4a3c33df9dee42
Gideon <69042336+noedigsti@users.noreply.github.com>
2022-05-02 05:22:58 +1000

    House keeping & Update

--- website/src/App.js
+++ website/src/App.js
@@ -1,37 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import profileImg from './profile.jpg';
+import {
+  BrowserRouter as Router,
+  // Routes,
+  // Route,
+} from 'react-router-dom'
+import ReactHelmet from 'react-helmet';
+import ReactRoundedImage from "react-rounded-image";
 
-const visitors = [
-  {
-    name: 'you',
-  },
-  {
-    name: 'me',
-  },
-]
-
-const Greeting = ({name}) => {
-  console.log(name);
+export default function App() {
   return (
-    <h1>
-      Welcome {name} to my page!
-    </h1>
-  )
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {visitors.map((visitor) => {
-          return <Greeting {...visitor} />
-        })}
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Site is in contruction! 🔨</p>
-      </header>
-    </div>
+    <Router>
+      <ReactHelmet title='Cuong Minh Vu | Home'/>
+      <div className="main-container">
+          <div className="profileImg">
+            <ReactRoundedImage 
+              image={profileImg} 
+              roundedColor="#04C2E0"
+              imageWidth="150"
+              imageHeight="150"
+              roundedSize="7"
+              borderRadius="70" 
+            />
+          </div>
+          <h1>
+            Hello there!
+          </h1>
+          <h2>
+            Welcome to my website!
+          </h2>
+          <p>
+            My name is Cuong Minh Vu
+          </p>
+          <div className="media-icons-wrapper">
+            <ul className="media-icons">
+              <li>
+                <a 
+                  className="link"
+                  href="https://linkedin.com/in/cuongminhvu"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/LinkedIn_logo.png" alt="linkedInIcon"/>
+                </a>
+              </li>
+              <li>
+                <a
+                  className="link"
+                  href="https://github.com/noedigsti"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src="/GitHub-Mark-Light-64px.png" alt="gitHubIcon"/>
+                </a>
+              </li>
+            </ul>
+          </div>
+      </div>
+    </Router>
   );
 }
 
-export default App;
--- website/src/index.js
+++ website/src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactHelmet from 'react-helmet';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ReactHelmet title='Cuong Minh Vu | Home'/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
