023071369e98fe0e6cb5554572ff8df070cd83ab
Gideon <69042336+noedigsti@users.noreply.github.com>
2022-05-10 06:26:15 +1000

    update

--- webserver/index.js
+++ webserver/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8008;
 
 const app = express();
+var jsonFile = require("./data.json"); // path of your json file
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
+  // res.json(jsonFile);
 });
 
 app.listen(PORT, () => {
--- website/src/pages/Query.js
+++ website/src/pages/Query.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import Layout from '../layouts/Layout'
+import React from "react";
+import Layout from "../layouts/Layout";
 
 export default function Query() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    // declare the data fetching function
+    const fetchData = async () => {
+      const data = await fetch("/api");
+      // convert data to json
+      // const json = await data.json();
+      // console.log(json);
+      // setData(JSON.stringify(json, null, 2));
+      console.log(data);
+    };
+    fetchData()
+      // make sure to catch any error
+      .catch(console.error);
+  }, []);
   return (
-    <Layout title={"Query"}>
-      <p>Query</p>
-    </Layout>
-  )
+    <>
+      <Layout title={"Query"}>
+        <p>Query</p>
+      </Layout>
+      <pre style={{textAlign: 'none', color: 'white'}}>{!data ? "Loading..." : data}</pre>
+    </>
+  );
 }
